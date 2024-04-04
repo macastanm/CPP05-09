8f115f294bdb4e6fe45491abb03036c3b8b7da05
macastan <mluizacmarques@gmail.com>
2024-04-04 12:47:57 +0100

    cpp 00 done

--- Module_05/ex00/main.cpp
+++ Module_05/ex00/main.cpp
@@ -17,7 +17,7 @@ int	main()
 	{
 		try
 		{
-			Bureaucrat	b("Cracked", 149);
+			Bureaucrat	b("Maria", 149);
 			std::cout << b;
 			b.decrementGrade();
 			std::cout << b;
@@ -25,15 +25,56 @@ int	main()
 			std::cout << b;
 			b.decrementGrade();
 			std::cout << b;
-			b.decrementGrade();
-			std::cout << b;
-			b.decrementGrade();
 		}
 		catch(const std::exception& e)
 		{
 			std::cerr << e.what() << std::endl;
 		}
-
+	}
+	{
+		try
+		{
+			Bureaucrat	a("Luiza", 2);
+			std::cout << a;
+			a.incrementGrade();
+			std::cout << a;
+			a.incrementGrade();
+			std::cout << a;
+			a.incrementGrade();
+			std::cout << a;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+	}
+	{
+		try
+		{
+			Bureaucrat	c("lulu", 180);
+			std::cout << c;
+			c.incrementGrade();
+			std::cout << c;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+	}
+	{
+		try
+		{
+			Bureaucrat	d("malu", 75);
+			std::cout << d;
+			d.incrementGrade();
+			std::cout << d;
+			d.decrementGrade();
+			std::cout << d;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
 	}
 	return(0);
 }
