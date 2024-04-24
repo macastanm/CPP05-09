dde764f652b2efb1c89090c5fc7c699d6f77199f
macastan <mluizacmarques@gmail.com>
2024-04-24 16:23:46 +0100

    ex02 done, have to check it

--- Module_07/ex02/Array.hpp
+++ Module_07/ex02/Array.hpp
@@ -10,4 +10,71 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
 
+# include <iostream>
+# include <stdio.h>
+# include <stdlib.h>
+
+template <typename T>
+class Array
+{
+private:
+	T	*array;
+	unsigned int	arraySize;
+public:
+	Array() : arraySize(0) {
+		array = new T[arraySize];
+	}
+	Array(unsigned int size) : arraySize(size) {
+		array = new T[arraySize];
+		for (unsigned int i = 0; i < size; i++)
+		{
+			array[i] = T();
+		}
+	}
+	Array(const Array& copy) : arraySize(copy.arraySize) {
+		array = new T[copy.arraySize];
+		for (unsigned int i = 0; i < arraySize; i++)
+		{
+			array[i] = T();
+		}
+	}
+	Array &operator=(const Array& src) {
+		if (this != src)
+		{
+			delete [] array;
+			arraySize = src.arraySize;
+			array = new T[src.arraySize];
+			for (unsigned int i = 0; i < arraySize; i++)
+			{
+				array[i] = T();
+			}
+		}
+		return (*this);
+	}
+	~Array() {
+		if (array)
+		{
+			delete [] array;
+		}
+	}
+	class outOfBounds : public std::exception
+	{
+	public:
+		virtual const char * what() const throw() {return("index is out of bounds");}
+	};
+	T &operator[](unsigned int n) {
+		if (n >= arraySize)
+		{
+			throw outOfBounds();
+		}
+		return (array[n]);
+	}
+	unsigned int	size(void) {
+		return (arraySize);
+	}
+};
+
+#endif
