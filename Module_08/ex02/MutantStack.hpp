e8cf1f5cfc7b89c77c38c0d360d821d979b93f05
macastan <mluizacmarques@gmail.com>
2024-05-15 17:13:23 +0100

    ex02 done

--- Module_08/ex02/MutantStack.hpp
+++ Module_08/ex02/MutantStack.hpp
@@ -10,4 +10,38 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
 
+# include <iostream>
+# include <algorithm>
+# include <stack>
+# include <limits>
+
+template <class T>
+class	MutantStack : public std::stack<T>
+{
+public:
+	MutantStack(){
+	}
+	MutantStack(const MutantStack& copy){
+		*this = copy;
+	}
+	MutantStack &operator=(const MutantStack& src){
+		std::stack<T>::operator=(this, src);
+		return (*this);
+	}
+	~MutantStack(){
+	}
+
+	typedef typename std::stack<T>::container_type::iterator it;
+
+	it	begin(void){
+		return (this->c.begin());
+	}
+	it	end(void){
+		return (this->c.end());
+	}
+};
+
+#endif
--- Module_08/ex02/main.cpp
+++ Module_08/ex02/main.cpp
@@ -11,18 +11,18 @@
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+#include <list>
 
 int main() {
 	MutantStack<int> mstack;
 	mstack.push(5);
 	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
+	std::cout << mstack.top() << std::endl << std::endl;
 	mstack.pop();
-	std::cout << mstack.size() << std::endl;
+	std::cout << mstack.size() << std::endl << std::endl;
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-//[...]
 	mstack.push(0);
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
@@ -33,5 +33,26 @@ int main() {
 		++it;
 	}
 	std::stack<int> s(mstack);
+
+	std::cout << std::endl << "-------------------------------------------------" << std::endl;
+	std::list<int> mlist;
+	mlist.push_back(5);
+	mlist.push_back(17);
+	std::cout << mlist.back() << std::endl << std::endl;
+	mlist.pop_back();
+	std::cout << mlist.size() << std::endl << std::endl;
+	mlist.push_back(3);
+	mlist.push_back(5);
+	mlist.push_back(737);
+	mlist.push_back(0);
+	std::list<int>::iterator it2 = mlist.begin();
+	std::list<int>::iterator ite2 = mlist.end();
+	++it2;
+	--it2;
+	while (it2 != ite2) {
+		std::cout << *it2 << std::endl;
+		++it2;
+	}
+	std::list<int> l(mlist);
 	return 0;
 }
