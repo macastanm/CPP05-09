ef0abffacc1a33e65c20136aa80ba58a2a4491e8
macastan <mluizacmarques@gmail.com>
2024-05-20 16:52:05 +0100

    ex00 start

--- Module_09/ex00/main.cpp
+++ Module_09/ex00/main.cpp
@@ -10,4 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "BitcoinExchange.hpp"
 
+int	main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		std::cout << "Error: could not open file." << std::endl;
+		return (1);
+	}
+	else
+	{
+
+	}
+	return (0);
+}
